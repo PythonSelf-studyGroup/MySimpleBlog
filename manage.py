faff4fbc2d264e5ec88d7340e68c0f6948d0cb01
shimchy <shimchy@gmail.com>
2021-01-12 19:38:22 +0800

    autoformat PEP8

--- manage.py
+++ manage.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-#==============================================================================
 #  coding by ShimchY shimchy@gmail.com and ... 2017
-#==============================================================================
+
 import os
 from app import create_app, db
 from app.models import User, Role
@@ -13,19 +12,21 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
+
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
+
+
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 #@app.errorhandler(404)
-#def page_not_found(e):
+# def page_not_found(e):
 #    return render_template('404.html'), 404
 
 #@app.errorhandler(500)
-#def internal_server_error(e):
+# def internal_server_error(e):
 #    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     manager.run()
-# commit
